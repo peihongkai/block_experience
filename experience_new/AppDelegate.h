a30b9a43aa94f23f813962a8e63f0cb016444aa8
裴洪凯 <peihongkai@peimatoMacBook-Air.local>
2012-09-27 15:07:32 +0800

    Initial Commit

---
+++ experience_new/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  experience_new
+//
+//  Created by 裴洪凯 on 12-9-27.
+//  Copyright (c) 2012年 裴洪凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
